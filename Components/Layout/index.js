f100e644b92a50cd6648b737c359615682fff6d5
Scott Wilson <swrs@hotmail.co.uk>
2022-04-13 15:09:10 +0100

    Made the welcome section into its own component

--- Components/Layout/index.js
+++ Components/Layout/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../../styles/Home.module.css";
 import NavBar from "../NavBar";
 import Projects from "../Projects";
 import "antd/dist/antd.css";
+import Welcome from "../Welcome";
 
 function Layout() {
   return (
@@ -14,27 +13,14 @@ function Layout() {
       <hr></hr>
       <br></br>
       <main className={styles.main}>
-        <div className={styles.titleBox}>
-          <div>
-            <h1 className={styles.title}>Hi, I am Scott Wilson.</h1>
-            <h2>Welcome to my portfolio</h2>
-          </div>
-          <div>
-            <Image
-              className={styles.imageBox}
-              alt="A picture of me"
-              height={200}
-              width={200}
-              src="https://i.imgur.com/A212bxt.jpg"
-            />
-          </div>
-        </div>
+        <Welcome />
+
         <div className={styles.aboutBox}>
           <h2 className={styles.title}>About Me:</h2>
           <h6>about me text</h6>
         </div>
         <br></br>
-        <hr></hr>
+        <br></br>
         <br></br>
         <Projects />
       </main>
---
+++ Components/Welcome/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "next/image";
+import css from "./style.module.css";
+
+function Welcome() {
+  return (
+    <div className={css.titleBox}>
+      <div>
+        <h1 className={css.title}>Hi, I am Scott Wilson.</h1>
+        <h2>Welcome to my portfolio</h2>
+      </div>
+      <div>
+        <Image
+          className={css.imageBox}
+          alt="A picture of me"
+          height={200}
+          width={200}
+          src="https://i.imgur.com/A212bxt.jpg"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
